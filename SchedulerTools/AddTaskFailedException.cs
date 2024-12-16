cd958a00804899a6090368ec1ccb71b4affb4e4b
David Le Bansais <dlebansais@gmail.com>
2024-12-16 21:08:02 +0100

    Update to .NET 9 and C# 13.

--- SchedulerTools/AddTaskFailedException.cs
+++ SchedulerTools/AddTaskFailedException.cs
@@ -6,7 +6,9 @@ using System;
 /// Represents an exception thrown when the API could not create the icon.
 /// </summary>
 [Serializable]
+#pragma warning disable CA1032 // Implement standard exception constructors
 public class AddTaskFailedException : Exception
+#pragma warning restore CA1032 // Implement standard exception constructors
 {
     /// <summary>
     /// Initializes a new instance of the <see cref="AddTaskFailedException"/> class.
